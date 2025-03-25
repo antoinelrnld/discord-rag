6a4487e0a79f73f8296413c090366d4c04bf94d8
antoinelrnld <38052726+antoinelrnld@users.noreply.github.com>
2025-03-25 23:22:48 +0100

    feat(metadata): sort context by timestamp at prompt generation

--- production/api/src/api/inference/prompting.py
+++ production/api/src/api/inference/prompting.py
@@ -2,10 +2,11 @@ from langchain_core.prompts import PromptTemplate
 
 TEMPLATE = """Utilise les éléments de contexte suivants pour répondre à la question à la fin.
 Les phrases suivantes sont des messages issus d'une conversation sur Discord.
-Il y a plusieurs intervenants, et les messages sont dans l'ordre chronologique.
+Il y a plusieurs intervenants, et les messages du contexte sont dans l'ordre chronologique.
+Répond en utilisant les éléments de contexte les plus récents si cela est pertinent.
 
 
-{context}
+Contexte : {context}
 
 Question : {question}
 
