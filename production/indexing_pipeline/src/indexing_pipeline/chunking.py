7a5f1e8d5e478ab97757d81d814540ebf56941a6
antoinelrnld <38052726+antoinelrnld@users.noreply.github.com>
2026-01-15 22:29:51 +0100

    chore: remove unused chunk_documents function

--- production/indexing_pipeline/src/indexing_pipeline/chunking.py
+++ production/indexing_pipeline/src/indexing_pipeline/chunking.py
@@ -1,5 +1,4 @@
 from langchain_experimental.text_splitter import SemanticChunker
-from langchain_core.documents import Document
 from langchain_openai import OpenAIEmbeddings
 
 chunker = SemanticChunker(
@@ -9,6 +8,3 @@ chunker = SemanticChunker(
     sentence_split_regex=r"<MESSAGE_SEP>",
     add_start_index=False
 )
-
-def chunk_documents(documents: list[Document]) -> list[Document]:
-    return chunker.split_documents(documents)
