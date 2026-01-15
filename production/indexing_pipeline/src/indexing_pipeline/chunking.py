a673dab5f12a3c05590364ba6f90e3f8c0c7f1c2
antoinelrnld <38052726+antoinelrnld@users.noreply.github.com>
2026-01-16 00:42:56 +0100

    feat(settings): setup settings management for indexing_pipeline

--- production/indexing_pipeline/src/indexing_pipeline/chunking.py
+++
@@ -1,8 +0,0 @@
-from langchain_experimental.text_splitter import SemanticChunker
-from langchain_openai import OpenAIEmbeddings
-
-chunker: SemanticChunker = SemanticChunker(
-    embeddings=OpenAIEmbeddings(model="text-embedding-3-large"),
-    sentence_split_regex=r"<MESSAGE_SEP>",
-    add_start_index=False,
-)
